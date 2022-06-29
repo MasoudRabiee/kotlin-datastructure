02c0df5a618b3eff32dd2bcc4541cc6b139364ef
MasoudRabiee <masoud.rabiee20@gmail.com>
2022-06-29 12:53:35 +0430

    Add ILinkedList.kt

---
+++ src/commonMain/kotlin/dataStructureKT/linkedList/ILinkedList.kt
@@ -0,0 +1,12 @@
+package dataStructureKT.linkedList
+
+interface ILinkedList<T> {
+    fun isEmpty(): Boolean
+    fun first(): T
+    fun last(): T
+    fun addFirst(element: T)
+    fun addLast(element: T)
+    fun removeFirst(): T?
+    fun removeLast(): T?
+    fun clear()
+}
